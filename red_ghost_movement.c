3f506e56de2896aae422054db6dfad7021a40b2d
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-27 09:32:21 +0400

    Update the red ghost algorithm

--- red_ghost_movement.c
+++ red_ghost_movement.c
@@ -6,7 +6,7 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 07:56:19 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/27 08:53:04 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/27 09:30:59 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,69 @@
 
 void shuffle(int *array, int n)
 {
-    for (int i = n - 1; i > 0; i--)
-    {
-        int j = rand() % (i + 1);
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+    int	i;
+	int	j;
+	int	temp;
+	
+	i = n - 1;
+	while (i > 0)
+	{
+		j = rand() % (i + 1);
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		i--;
+	}
+}
+
+int get_opposite_direction(int direction)
+{
+	if (direction == 0)
+		return (2);
+	else if (direction == 1)
+		return (3);
+	else if (direction == 2)
+		return (0);
+	else if (direction == 3)
+		return (1);
+	return (-1);
+}
+
+void get_available_directions(int *available_directions, int direction)
+{
+	int	i;
+	int count;
+	int opposite_direction;
+    int	directions[4] = {0, 1, 2, 3};
+	
+    shuffle(directions, 4);
+    opposite_direction = get_opposite_direction(direction);
+	i = 0;
+    count = 0;
+	while (i < 4)
+	{
+		if (directions[i] != opposite_direction)
+		{
+			available_directions[count] = directions[i];
+			count++;
+		}
+		if (count == 3)
+			break;
+		i++;
+	}
+}
+
+int is_valid_move(t_params *params, int row, int col, int direction)
+{
+    if (direction == 0 && row > 0 && params->map_data[row - 1][col] != '1')
+        return 1; // Up is a valid move
+    else if (direction == 1 && col < 22 && params->map_data[row][col + 1] != '1')
+        return 1; // Right is a valid move
+    else if (direction == 2 && row < 22 && params->map_data[row + 1][col] != '1')
+        return 1; // Down is a valid move
+    else if (direction == 3 && col > 0 && params->map_data[row][col - 1] != '1')
+        return 1; // Left is a valid move
+    return 0; // Invalid move
 }
 
 int has_multiple_options(t_params *params, int row, int col)
@@ -54,68 +110,45 @@ int has_multiple_options(t_params *params, int row, int col)
 	return (0);
 }
 
+int	choose_valid_direction(t_params *params, int new_col, int new_row, int direction)
+{
+	if (direction == 0 && new_row > 0 && params->map_data[new_row - 1][new_col] != '1')
+		new_row--;
+	else if (direction == 1 && new_col < 22 && params->map_data[new_row][new_col + 1] != '1')
+		new_col++;
+	else if (direction == 2 && new_row < 22 && params->map_data[new_row + 1][new_col] != '1')
+		new_row++;
+	else if (direction == 3 && new_col > 0 && params->map_data[new_row][new_col - 1] != '1')
+		new_col--;
+	if (new_col != params->red.col || new_row != params->red.row)
+	{
+		params->red.col = new_col;
+		params->red.row = new_row;
+		params->red.direction = direction;
+		return (1);
+	}
+	return (0);
+	
+}
+
 void	move_red_ghost_random(t_params *params)
 {
-	int i;
-    int new_col;
-    int new_row;
-    int direction;
-    int available_directions[3]; // Array to store available directions (excluding opposite)
-
-    int directions[4] = {0, 1, 2, 3};
-
-    // Determine the opposite direction of the current direction of the red ghost
-    int opposite_direction;
-    switch (params->red.direction)
-    {
-        case 0: // Up
-            opposite_direction = 2; // Down
-            break;
-        case 1: // Right
-            opposite_direction = 3; // Left
-            break;
-        case 2: // Down
-            opposite_direction = 0; // Up
-            break;
-        case 3: // Left
-            opposite_direction = 1; // Right
-            break;
-    }
-
-    // Create an array of available directions (excluding opposite)
-    int count = 0;
-    for (i = 0; i < 4; i++)
-    {
-        if (directions[i] != opposite_direction)
-        {
-            available_directions[count] = directions[i];
-            count++;
-        }
-    }
-
-    // Now we shuffle the available directions array
-    shuffle(available_directions, 3);
+	int	i;
+	int	new_col;
+	int	new_row;
+	int	direction;
+	int available_directions[3];
+	
 	i = 0;
+    shuffle(available_directions, 3);
+	get_available_directions(available_directions, params->red.direction);
 	while (i < 3)
 	{
 		direction = available_directions[i];
 		new_col = params->red.col;
 		new_row = params->red.row;
-		if (direction == 0 && new_row > 0 && params->map_data[new_row - 1][new_col] != '1')
-			new_row--;
-		else if (direction == 1 && new_col < 22 && params->map_data[new_row][new_col + 1] != '1')
-			new_col++;
-		else if (direction == 2 && new_row < 22 && params->map_data[new_row + 1][new_col] != '1')
-			new_row++;
-		else if (direction == 3 && new_col > 0 && params->map_data[new_row][new_col - 1] != '1')
-			new_col--;
-		if (new_col != params->red.col || new_row != params->red.row)
-		{
-			params->red.col = new_col;
-			params->red.row = new_row;
-			params->red.direction = direction;
+		if (choose_valid_direction(params, new_col, new_row, direction))
 			break;
-		}
 		i++;
 	}
 }
@@ -141,7 +174,7 @@ void	move_red_ghost_continue(t_params *params)
 
 void	move_red_ghost(t_params *params)
 {
-	// int has_multiple;
+	int has_multiple;
     static int counter = 0;
     static int ghost_speed = 20;
 
@@ -150,7 +183,7 @@ void	move_red_ghost(t_params *params)
     {
         counter = 0;
 
-        int has_multiple = has_multiple_options(params, params->red.row, params->red.col);
+        has_multiple = has_multiple_options(params, params->red.row, params->red.col);
         if (has_multiple)
             move_red_ghost_random(params);
         else
