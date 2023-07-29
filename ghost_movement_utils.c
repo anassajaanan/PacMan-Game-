985ab881052b731a33eb9ba36c45eab56657129f
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-29 11:46:03 +0400

    Add the norm to ghost_movement.c

--- ghost_movement_utils.c
+++ ghost_movement_utils.c
@@ -6,18 +6,18 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:34:00 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/28 19:57:11 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/29 11:31:57 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void shuffle(int *array, int n)
+void	shuffle(int *array, int n)
 {
-    int	i;
+	int	i;
 	int	j;
 	int	temp;
-	
+
 	i = n - 1;
 	while (i > 0)
 	{
@@ -29,7 +29,7 @@ void shuffle(int *array, int n)
 	}
 }
 
-int get_opposite_direction(int direction)
+int	get_opposite_direction(int direction)
 {
 	if (direction == 0)
 		return (2);
@@ -42,17 +42,17 @@ int get_opposite_direction(int direction)
 	return (-1);
 }
 
-void get_available_directions(int *available_directions, int direction)
+void	get_available_directions(int *available_directions, int direction)
 {
 	int	i;
-	int count;
-	int opposite_direction;
-    int	directions[4] = {0, 1, 2, 3};
-	
-    shuffle(directions, 4);
-    opposite_direction = get_opposite_direction(direction);
+	int	count;
+	int	opposite_direction;
+	int	directions[4] = {0, 1, 2, 3};
+
+	shuffle(directions, 4);
+	opposite_direction = get_opposite_direction(direction);
 	i = 0;
-    count = 0;
+	count = 0;
 	while (i < 4)
 	{
 		if (directions[i] != opposite_direction)
@@ -61,31 +61,33 @@ void get_available_directions(int *available_directions, int direction)
 			count++;
 		}
 		if (count == 3)
-			break;
+			break ;
 		i++;
 	}
 }
 
-int is_valid_move(t_params *params, int row, int col, int direction)
+int	is_valid_move(t_params *params, int row, int col, int direction)
 {
-    if (direction == 0 && row > 0 && params->map.data[row - 1][col] != '1')
-        return 1;
-    else if (direction == 1 && col < params->map.cols - 1 && params->map.data[row][col + 1] != '1')
-        return 1;
-    else if (direction == 2 && row < params->map.rows - 1 && params->map.data[row + 1][col] != '1')
-        return 1;
-    else if (direction == 3 && col > 0 && params->map.data[row][col - 1] != '1')
-        return 1;
-    return 0;
+	if (direction == 0 && row > 0 && params->map.data[row - 1][col] != '1')
+		return (1);
+	else if (direction == 1 && col < params->map.cols - 1
+		&& params->map.data[row][col + 1] != '1')
+		return (1);
+	else if (direction == 2 && row < params->map.rows - 1
+		&& params->map.data[row + 1][col] != '1')
+		return (1);
+	else if (direction == 3 && col > 0 && params->map.data[row][col - 1] != '1')
+		return (1);
+	return (0);
 }
 
-int has_multiple_options(t_params *params, int row, int col)
+int	has_multiple_options(t_params *params, int row, int col)
 {
 	int	i;
-    int	options;
+	int	options;
 	int	new_col;
 	int	new_row;
-    int	directions[4] = {0, 1, 2, 3}; // Array of possible directions (0: up, 1: right, 2: down, 3: left)
+	int	directions[4] = {0, 1, 2, 3};
 
 	i = 0;
 	options = 0;
@@ -93,13 +95,17 @@ int has_multiple_options(t_params *params, int row, int col)
 	{
 		new_col = col;
 		new_row = row;
-		if (directions[i] == 0 && new_row > 0 && params->map.data[new_row - 1][new_col] != '1')
+		if (directions[i] == 0 && new_row > 0 && params->map.data[new_row - 1]
+			[new_col] != '1')
 			new_row--;
-		else if (directions[i] == 1 && new_col < params->map.cols - 1 && params->map.data[new_row][new_col + 1] != '1')
+		else if (directions[i] == 1 && new_col < params->map.cols - 1
+			&& params->map.data[new_row][new_col + 1] != '1')
 			new_col++;
-		else if (directions[i] == 2 && new_row < params->map.rows - 1 && params->map.data[new_row + 1][new_col] != '1')
+		else if (directions[i] == 2 && new_row < params->map.rows - 1
+			&& params->map.data[new_row + 1][new_col] != '1')
 			new_row++;
-		else if (directions[i] == 3 && new_col > 0 && params->map.data[new_row][new_col - 1] != '1')
+		else if (directions[i] == 3 && new_col > 0
+			&& params->map.data[new_row][new_col - 1] != '1')
 			new_col--;
 		if (new_col != col || new_row != row)
 			options++;
