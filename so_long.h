2813e521595b9707cbe10552156ebb511a3fe415
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-29 09:22:23 +0400

    Add the norm to so_long.h

--- so_long.h
+++ so_long.h
@@ -6,81 +6,77 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:27:21 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/29 08:21:19 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/29 09:21:40 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#define WIN_WIDTH 736
-#define WIN_HEIGHT 736
-
-# include "./mlx/mlx.h"
-# include "./libft/libft.h"
 # include "./ft_printf/ft_printf.h"
 # include "./get_next_line/get_next_line.h"
-
+# include "./libft/libft.h"
+# include "./mlx/mlx.h"
+# include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
 typedef struct s_player
 {
-	int		col;
-	int		row;
-	int		score;
-	int		moves;
-	void	*img[4];
-	void	*animation_img;
-	int		direction;
-}			t_player;
+	int							col;
+	int							row;
+	int							score;
+	int							moves;
+	void						*img[4];
+	void						*animation_img;
+	int							direction;
+}								t_player;
 
 typedef struct s_exit
 {
-	int col;
-	int row;
-}       t_exit;
+	int							col;
+	int							row;
+}								t_exit;
 
 typedef struct s_ghost
 {
-	int		col;
-	int		row;
-	void	*img[4];
-	int		direction;
-}			t_ghost;
+	int							col;
+	int							row;
+	void						*img[4];
+	int							direction;
+}								t_ghost;
 
 typedef struct s_fruit
 {
-	int		col;
-	int		row;
-	void	*img;
-}			t_fruit;
+	int							col;
+	int							row;
+	void						*img;
+}								t_fruit;
 
 typedef struct s_images
 {
-	void	*wall;
-	void	*collectible;
-	void	*exit;
-	void	*empty;
-	void	*img_log;
-	void	*game_over;
-	void	*win;
-	void	*exit1;
-	void	*exit2;
-}			t_images;
+	void						*wall;
+	void						*collectible;
+	void						*exit;
+	void						*empty;
+	void						*img_log;
+	void						*game_over;
+	void						*win;
+	void						*exit1;
+	void						*exit2;
+}								t_images;
 
 typedef struct s_map
 {
-	int         rows;
-	int         cols;
-	char        **data;
-	int			collectibles;
-	t_exit		target;
-}				t_map;
+	int							rows;
+	int							cols;
+	char						**data;
+	int							collectibles;
+	t_exit						target;
+}								t_map;
 
-struct s_line_queue_node
+struct							s_line_queue_node
 {
 	char						*val;
 	struct s_line_queue_node	*next;
@@ -94,114 +90,123 @@ typedef struct s_line_queue
 
 typedef struct s_params
 {
-	void			*mlx;
-	void			*win;
-	t_map			map;
-	t_line_queue	q;
-	t_player		player;
-	t_exit			exit;
-	t_ghost			red;
-	t_ghost			blue;
-	t_ghost			yellow;
-	t_ghost			pink;
-	t_fruit			apple;
-	t_fruit			orange;
-	t_fruit			cherry;
-	t_fruit			strawberry;
-	t_images		images;
-	int				is_game_over;
-	int				is_win;
-}					t_params;
-
+	void						*mlx;
+	void						*win;
+	t_map						map;
+	t_line_queue				q;
+	t_player					player;
+	t_exit						exit;
+	t_ghost						red;
+	t_ghost						blue;
+	t_ghost						yellow;
+	t_ghost						pink;
+	t_fruit						apple;
+	t_fruit						orange;
+	t_fruit						cherry;
+	t_fruit						strawberry;
+	t_images					images;
+	int							is_game_over;
+	int							is_win;
+}								t_params;
 
 // player.c
-void	init_and_load_player(t_params *params);
-void	check_ghost_collision(t_params *params);
-void	check_fruit_collision(t_params *params);
+void							init_and_load_player(t_params *params);
+void							check_ghost_collision(t_params *params);
+void							check_fruit_collision(t_params *params);
 
 // ghosts.c
-void	init_ghosts(t_params *params);
-void	load_ghosts_imgs(t_params *params);
-void	draw_ghosts(t_params *params);
+void							init_ghosts(t_params *params);
+void							load_ghosts_imgs(t_params *params);
+void							draw_ghosts(t_params *params);
 
 // fruits.c
-void	init_and_load_fruits(t_params *params);
-void	draw_fruits(t_params *params);
+void							init_and_load_fruits(t_params *params);
+void							draw_fruits(t_params *params);
 
 // images.c
-void	load_images(t_params *params);
-void	draw_images(t_params *params);
+void							load_images(t_params *params);
+void							draw_images(t_params *params);
 
 // parsing.c
-void	parse_map_data(t_params *params);
+void							parse_map_data(t_params *params);
 
 // mouvements.c
-int		is_valid_mov(t_params *params, int row, int col);
-void	move_player_up(t_params *params);
-void	move_player_left(t_params *params);
-void	move_player_down(t_params *params);
-void	move_player_right(t_params *params);
+int								is_valid_mov(t_params *params, int row,
+									int col);
+void							move_player_up(t_params *params);
+void							move_player_left(t_params *params);
+void							move_player_down(t_params *params);
+void							move_player_right(t_params *params);
 
 // ghost_movement.c
-void	move_pink_ghost(t_params *params);
-void	move_yellow_ghost(t_params *params);
+void							move_pink_ghost(t_params *params);
+void							move_yellow_ghost(t_params *params);
 
 // red_ghost_movement.c
-int		choose_valid_direction_red(t_params *params, int new_col, int new_row, int direction);
-void	move_red_ghost_random(t_params *params);
-void	move_red_ghost_continue(t_params *params);
-void	move_red_ghost(t_params *params);
+int								choose_valid_direction_red(t_params *params,
+									int new_col, int new_row, int direction);
+void							move_red_ghost_random(t_params *params);
+void							move_red_ghost_continue(t_params *params);
+void							move_red_ghost(t_params *params);
 
 // blue_ghost_movement.c
-int		choose_valid_direction_blue(t_params *params, int new_col, int new_row, int direction);
-void	move_blue_ghost_random(t_params *params);
-void	move_blue_ghost_continue(t_params *params);
-void	move_blue_ghost(t_params *params);
-
+int								choose_valid_direction_blue(t_params *params,
+									int new_col, int new_row, int direction);
+void							move_blue_ghost_random(t_params *params);
+void							move_blue_ghost_continue(t_params *params);
+void							move_blue_ghost(t_params *params);
 
 // ghost_movement_utils.c
-void	shuffle(int *array, int n);
-int		get_opposite_direction(int direction);
-void	get_available_directions(int *available_directions, int direction);
-int		is_valid_move(t_params *params, int row, int col, int direction);
-int		has_multiple_options(t_params *params, int row, int col);
+void							shuffle(int *array, int n);
+int								get_opposite_direction(int direction);
+void							get_available_directions(
+									int *available_directions, int direction);
+int								is_valid_move(t_params *params, int row,
+									int col, int direction);
+int								has_multiple_options(t_params *params, int row,
+									int col);
 
 // line_queue.c
-void	init_line_queue(t_line_queue *q);
-void	enqueue_line(t_line_queue *q, char *line);
-char	*dequeue_line(t_line_queue *q);
-int		line_queue_is_empty(t_line_queue *q);
-int     line_queue_size(t_line_queue *q);
+void							init_line_queue(t_line_queue *q);
+void							enqueue_line(t_line_queue *q, char *line);
+char							*dequeue_line(t_line_queue *q);
+int								line_queue_is_empty(t_line_queue *q);
+int								line_queue_size(t_line_queue *q);
 
 // map_validator.c
-int is_valid_character(char c);
-int count_charcters(t_map *map, char c);
-int is_valid_map_format(t_map *map);
-int is_valid_map(t_map *map);
-int has_duplicates(t_map *map);
-int is_map_rectangular(t_map *map);
-int is_row_only_walls(char *row);
-int is_map_closed_by_walls(t_map *map);
-int	has_valid_path_to_collectibles(t_params *params);
-int validate_map(t_params *params);
+int								is_valid_character(char c);
+int								count_charcters(t_map *map, char c);
+int								is_valid_map_format(t_map *map);
+int								is_valid_map(t_map *map);
+int								has_duplicates(t_map *map);
+int								is_map_rectangular(t_map *map);
+int								is_row_only_walls(char *row);
+int								is_map_closed_by_walls(t_map *map);
+int								has_valid_path_to_collectibles(
+									t_params *params);
+int								validate_map(t_params *params);
 
 // map.c
-void    init_map(t_map *map);
-void    parse_map_from_queue(t_line_queue *q, t_map *map);
-void	parse_map(t_params *params);
-void    find_player_and_exit_position(t_params *params);
-int		count_collectibles(t_map *map);
-void    free_map(t_map *map);
+void							init_map(t_map *map);
+void							parse_map_from_queue(t_line_queue *q,
+									t_map *map);
+void							parse_map(t_params *params);
+void							find_player_and_exit_position(t_params *params);
+int								count_collectibles(t_map *map);
+void							free_map(t_map *map);
 
 // depth_first_search.c
-int is_visited(int visited[][2], int *index, int col, int row);
-void    mark_visited(int visited[][2], int *index, int col, int row);
-int valid_move(t_map *map, int *new_pos, int visited[][2], int *index);
-int depth_first_search(t_map *map, int *current_pos, int visited[][2], int *index);
-int has_valid_path(t_params *params);
+int								is_visited(int visited[][2], int *index,
+									int col, int row);
+void							mark_visited(int visited[][2], int *index,
+									int col, int row);
+int								valid_move(t_map *map, int *new_pos,
+									int visited[][2], int *index);
+int								depth_first_search(t_map *map, int *current_pos,
+									int visited[][2], int *index);
+int								has_valid_path(t_params *params);
 
-
-
-void	get_random_collectible_position(t_params *params, int *col, int *row);
+void							get_random_collectible_position(
+									t_params *params, int *col, int *row);
 
 #endif // SO_LONG_H
