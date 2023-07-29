991ba2436d2d9f85c03c38342ab47ea4287e566f
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-29 09:54:40 +0400

    Add the norm to blue_ghost_algorithm.c

--- blue_ghost_algorithm.c
+++ blue_ghost_algorithm.c
@@ -6,21 +6,26 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:41:19 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/28 19:58:49 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/29 09:53:21 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h" 
+#include "so_long.h"
 
-int	choose_valid_direction_blue(t_params *params, int new_col, int new_row, int direction)
+int	choose_valid_direction_blue(t_params *params, int new_col, int new_row,
+		int direction)
 {
-	if (direction == 0 && new_row > 0 && params->map.data[new_row - 1][new_col] != '1')
+	if (direction == 0 && new_row > 0 && params->map.data[new_row - 1]
+		[new_col] != '1')
 		new_row--;
-	else if (direction == 1 && new_col < params->map.cols - 1 && params->map.data[new_row][new_col + 1] != '1')
+	else if (direction == 1 && new_col < params->map.cols - 1
+		&& params->map.data[new_row][new_col + 1] != '1')
 		new_col++;
-	else if (direction == 2 && new_row < params->map.rows - 1 && params->map.data[new_row + 1][new_col] != '1')
+	else if (direction == 2 && new_row < params->map.rows - 1
+		&& params->map.data[new_row + 1][new_col] != '1')
 		new_row++;
-	else if (direction == 3 && new_col > 0 && params->map.data[new_row][new_col - 1] != '1')
+	else if (direction == 3 && new_col > 0 && params->map.data[new_row]
+		[new_col - 1] != '1')
 		new_col--;
 	if (new_col != params->blue.col || new_row != params->blue.row)
 	{
@@ -38,10 +43,10 @@ void	move_blue_ghost_random(t_params *params)
 	int	new_col;
 	int	new_row;
 	int	direction;
-	int available_directions[3];
-	
+	int	available_directions[3];
+
 	i = 0;
-    shuffle(available_directions, 3);
+	shuffle(available_directions, 3);
 	get_available_directions(available_directions, params->blue.direction);
 	while (i < 3)
 	{
@@ -49,25 +54,29 @@ void	move_blue_ghost_random(t_params *params)
 		new_col = params->blue.col;
 		new_row = params->blue.row;
 		if (choose_valid_direction_blue(params, new_col, new_row, direction))
-			break;
+			break ;
 		i++;
 	}
 }
 
 void	move_blue_ghost_continue(t_params *params)
 {
-	int new_col;
-	int new_row;
+	int	new_col;
+	int	new_row;
 
 	new_col = params->blue.col;
 	new_row = params->blue.row;
-	if (params->blue.direction == 0 && new_row > 0 && params->map.data[new_row - 1][new_col] != '1')
+	if (params->blue.direction == 0 && new_row > 0 && params->map.data
+		[new_row - 1][new_col] != '1')
 		new_row--;
-	else if (params->blue.direction == 1 && new_col < params->map.cols - 1 && params->map.data[new_row][new_col + 1] != '1')
+	else if (params->blue.direction == 1 && new_col < params->map.cols - 1
+		&& params->map.data[new_row][new_col + 1] != '1')
 		new_col++;
-	else if (params->blue.direction == 2 && new_row < params->map.rows - 1 && params->map.data[new_row + 1][new_col] != '1')
+	else if (params->blue.direction == 2 && new_row < params->map.rows - 1
+		&& params->map.data[new_row + 1][new_col] != '1')
 		new_row++;
-	else if (params->blue.direction == 3 && new_col > 0 && params->map.data[new_row][new_col - 1] != '1')
+	else if (params->blue.direction == 3 && new_col > 0
+		&& params->map.data[new_row][new_col - 1] != '1')
 		new_col--;
 	params->blue.row = new_row;
 	params->blue.col = new_col;
@@ -75,15 +84,16 @@ void	move_blue_ghost_continue(t_params *params)
 
 void	move_blue_ghost(t_params *params)
 {
-	int has_multiple;
-	static int counter = 0;
-	static int ghost_speed = 20;
+	int			has_multiple;
+	static int	counter = 0;
+	static int	ghost_speed = 20;
 
 	counter++;
 	if (counter >= ghost_speed)
 	{
 		counter = 0;
-		has_multiple = has_multiple_options(params, params->blue.row, params->blue.col);
+		has_multiple = has_multiple_options(params, params->blue.row,
+				params->blue.col);
 		if (has_multiple)
 			move_blue_ghost_random(params);
 		else
