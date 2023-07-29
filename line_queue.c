45e678bac70aa71abce4d6e420e06d002a14f80c
Anas Ajaanan <aajaanan@lab3r6s14.42abudhabi.ae>
2023-07-29 12:05:45 +0400

    Add the norm to line_queue.c

--- line_queue.c
+++ line_queue.c
@@ -6,7 +6,7 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:02:25 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/07/28 15:03:58 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/07/29 12:04:37 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ void	init_line_queue(t_line_queue *q)
 	q->rear = NULL;
 }
 
-void    enqueue_line(t_line_queue *q, char *line)
+void	enqueue_line(t_line_queue *q, char *line)
 {
 	struct s_line_queue_node	*new_node;
 
-	new_node = (struct s_line_queue_node *)malloc(sizeof(struct s_line_queue_node));
+	new_node = (struct s_line_queue_node *)malloc
+		(sizeof(struct s_line_queue_node));
 	new_node->val = line;
 	new_node->next = NULL;
 	if (q->front == NULL)
@@ -37,10 +38,10 @@ void    enqueue_line(t_line_queue *q, char *line)
 	}
 }
 
-char    *dequeue_line(t_line_queue *q)
+char	*dequeue_line(t_line_queue *q)
 {
 	struct s_line_queue_node	*tmp;
-	char                        *line;
+	char						*line;
 
 	tmp = q->front;
 	q->front = q->front->next;
@@ -49,17 +50,17 @@ char    *dequeue_line(t_line_queue *q)
 	return (line);
 }
 
-int     line_queue_is_empty(t_line_queue *q)
+int	line_queue_is_empty(t_line_queue *q)
 {
 	if (q->front == NULL)
 		return (1);
 	return (0);
 }
 
-int line_queue_size(t_line_queue *q)
+int	line_queue_size(t_line_queue *q)
 {
-	struct s_line_queue_node    *tmp;
-	int                         size;
+	struct s_line_queue_node	*tmp;
+	int							size;
 
 	size = 0;
 	tmp = q->front;
